1221b7610556797507d8d4717087fbd31e466d98
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-12-21 09:30:50 -0300

    first commit

--- profile-facebook/ViewController.swift
+++ profile-facebook/ViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: IBOutlet
+    
+    @IBOutlet weak var imageCoverProfile: UIImageView!
+    @IBOutlet weak var imagePerfilProfile: UIImageView!
+    @IBOutlet weak var buttonAddStory: UIButton!
+    @IBOutlet weak var buttonMoreDetails: UIButton!   
+    @IBOutlet weak var buttonTakePhotoCover: UIView!
+    @IBOutlet weak var buttonTakePhotoPerfil: UIView!
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    // MARK: Helpers
+    
+    func configureUI() {
+        self.imagePerfilProfile.layer.cornerRadius = imagePerfilProfile.frame.width / 2
+        self.imagePerfilProfile.layer.borderWidth = 3
+        self.imagePerfilProfile.layer.borderColor = UIColor.white.cgColor
+        
+        self.buttonAddStory.layer.cornerRadius = 5
+        self.buttonAddStory.setTitle("  Add to Story", for: .normal)
+        
+        self.buttonMoreDetails.layer.cornerRadius = 5
+        
+        self.imageCoverProfile.layer.cornerRadius = 5
+        self.imageCoverProfile.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        self.buttonTakePhotoCover.layer.cornerRadius = buttonTakePhotoCover.frame.width / 2
+        self.buttonTakePhotoPerfil.layer.cornerRadius = buttonTakePhotoPerfil.frame.width / 2
+        
     }
 
 
