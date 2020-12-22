3ed6bbd1cbd5642ffc0709e29254ab7ad840758e
AlanSoaresDeOliveira <alan.oliveira19@fatec.sp.gov.br>
2020-12-22 12:00:22 -0300

    Add Collection View to friends

---
+++ profile-facebook/FriendsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsCollectionViewCell.swift
+//  profile-facebook
+//
+//  Created by Alan Soares de Oliveira on 22/12/20.
+//
+
+import UIKit
+
+class FriendsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageFriend: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+}
--- profile-facebook/ViewController.swift
+++ profile-facebook/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
     
     // MARK: IBOutlet
     
@@ -17,17 +18,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonMoreDetails: UIButton!   
     @IBOutlet weak var buttonTakePhotoCover: UIView!
     @IBOutlet weak var buttonTakePhotoPerfil: UIView!
+    @IBOutlet weak var buttonEditPublicDetails: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var friendsCollection: UICollectionView!
     
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        friendsCollection.delegate = self
+        friendsCollection.dataSource = self
     }
     
     // MARK: Helpers
     
     func configureUI() {
+        
         self.imagePerfilProfile.layer.cornerRadius = imagePerfilProfile.frame.width / 2
         self.imagePerfilProfile.layer.borderWidth = 3
         self.imagePerfilProfile.layer.borderColor = UIColor.white.cgColor
@@ -43,8 +50,29 @@ class ViewController: UIViewController {
         self.buttonTakePhotoCover.layer.cornerRadius = buttonTakePhotoCover.frame.width / 2
         self.buttonTakePhotoPerfil.layer.cornerRadius = buttonTakePhotoPerfil.frame.width / 2
         
+        self.buttonEditPublicDetails.layer.cornerRadius = 5
+//        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width, height: self.scrollView.frame.height + 400)
+        
     }
 
+    // MARK: UIColletcionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "friendcell", for: indexPath) as! FriendsCollectionViewCell
+        
+        cell.backgroundColor = .blue
+        cell.layer.cornerRadius = 5
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return UIDevice.current.userInterfaceIdiom == .phone ? CGSize(width: collectionView.bounds.width / 3 - 8, height: 160) : CGSize(width: collectionView.bounds.width / 3 - 20, height: 250)
+    }
 
 }
 
